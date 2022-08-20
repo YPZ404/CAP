26e3bf95813fe70af4e57d29200bc06cccea4cf5
rton8097 <rton8097@uni.sydney.edu.au>
2022-08-20 22:10:34 +1000

    fixed the cancel buttons to stop the timer when recording and reset the text to "start". Changed the recording interval from 500ms -> 100ms

--- app/screens/BalanceTests/BTFour.jsx
+++ app/screens/BalanceTests/BTFour.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   Text,
   SafeAreaView,
@@ -6,27 +6,30 @@ import {
   TouchableOpacity,
   View,
   Vibration,
-} from 'react-native';
-import { Accelerometer } from 'expo-sensors';
+} from "react-native";
+import { Accelerometer } from "expo-sensors";
 
-import uiStyle from '../../components/uiStyle.jsx';
-import { useContext, useState } from 'react';
-import { dataContext } from '../../components/GlobalContextProvider';
-import getStandardDeviation from '../../model/standardDeviation';
+import uiStyle from "../../components/uiStyle.jsx";
+import { useContext, useState } from "react";
+import { dataContext } from "../../components/GlobalContextProvider";
+import getStandardDeviation from "../../model/standardDeviation";
 
 function BTFour({ navigation }) {
-  const [text, setText] = useState('Start!');
-  const changeText = () => setText('Recording!');
+  const [text, setText] = useState("Start!");
+  const startedText = () => setText("Recording!");
+  const resetText = () => setText("Start!");
   const [data2, setData2] = useContext(dataContext);
   const [subscription, setSubscription] = useState(null);
   const x_arr = [];
   const y_arr = [];
   const z_arr = [];
+  const [timer, setTimer] = useState(null);
+
   const _subscribe = () => {
     setSubscription(
       Accelerometer.addListener((accelerometerData) => {
         // setData(accelerometerData);
-        Accelerometer.setUpdateInterval(500);
+        Accelerometer.setUpdateInterval(100);
         x_arr.push(accelerometerData.x);
         y_arr.push(accelerometerData.y);
         z_arr.push(accelerometerData.z);
@@ -35,7 +38,7 @@ function BTFour({ navigation }) {
         const z_sd = getStandardDeviation(z_arr);
         const sd = (x_sd + y_sd + z_sd) / 3;
         setData2(sd);
-      }),
+      })
     );
   };
 
@@ -43,20 +46,24 @@ function BTFour({ navigation }) {
     <SafeAreaView style={uiStyle.container}>
       <Text style={uiStyle.stackedText}>
         Hold to chest for 10 seconds after clicking "Start!" while keeping one
-        leg up in the air. {'\n'}
-        {'\n'}
+        leg up in the air. {"\n"}
+        {"\n"}
       </Text>
       <TouchableOpacity
         onPress={() => {
           if (!subscription) {
             _subscribe();
+            startedText();
+            setTimer(
+              setTimeout(() => {
+                Accelerometer.removeAllListeners();
+                Vibration.vibrate();
+                setSubscription(null);
+                resetText();
+                navigation.navigate("Balance Test 5");
+              }, 10000)
+            );
           }
-          changeText();
-          setTimeout(() => {
-            Accelerometer.removeAllListeners();
-            Vibration.vibrate();
-            navigation.navigate('Balance Test 5');
-          }, 10000);
         }}
         style={styles.startCheckButton}
       >
@@ -64,7 +71,11 @@ function BTFour({ navigation }) {
       </TouchableOpacity>
       <View style={uiStyle.textContainer}>
         <TouchableOpacity
-          onPress={() => navigation.navigate('Balance Test 1')}
+          onPress={() => {
+            Accelerometer.removeAllListeners();
+            clearTimeout(timer);
+            navigation.navigate("Balance Test 1");
+          }}
           style={uiStyle.bottomButton}
         >
           <Text style={uiStyle.buttonLabel}>Cancel</Text>
@@ -74,37 +85,37 @@ function BTFour({ navigation }) {
   );
 }
 
-const title = '#000000';
-const text = '#fff';
-const background = '#fff';
-const buttons = '#ff3333';
+const title = "#000000";
+const text = "#fff";
+const background = "#fff";
+const buttons = "#ff3333";
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: background,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   startCheckButton: {
     width: 200,
     height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     borderRadius: 100,
     backgroundColor: buttons,
   },
   startCheckText: {
     color: text,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 20,
   },
   titleText: {
     color: title,
     fontSize: 30,
-    position: 'absolute',
+    position: "absolute",
     top: 60,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
--- app/screens/BalanceTests/BTTwo.jsx
+++ app/screens/BalanceTests/BTTwo.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   Text,
   SafeAreaView,
@@ -6,27 +6,30 @@ import {
   TouchableOpacity,
   View,
   Vibration,
-} from 'react-native';
-import { Accelerometer } from 'expo-sensors';
+} from "react-native";
+import { Accelerometer } from "expo-sensors";
 
-import uiStyle from '../../components/uiStyle.jsx';
-import { useContext, useState } from 'react';
-import { dataContext } from '../../components/GlobalContextProvider';
-import getStandardDeviation from '../../model/standardDeviation';
+import uiStyle from "../../components/uiStyle.jsx";
+import { useContext, useState } from "react";
+import { dataContext } from "../../components/GlobalContextProvider";
+import getStandardDeviation from "../../model/standardDeviation";
 
 function BTTwo({ navigation }) {
-  const [text, setText] = useState('Start!');
-  const changeText = () => setText('Recording!');
+  const [text, setText] = useState("Start!");
+  const startedText = () => setText("Recording!");
+  const resetText = () => setText("Start!");
   const [data, setData] = useContext(dataContext);
   const [subscription, setSubscription] = useState(null);
   const x_arr = [];
   const y_arr = [];
   const z_arr = [];
+  const [timer, setTimer] = useState(null);
+
   const _subscribe = () => {
     setSubscription(
       Accelerometer.addListener((accelerometerData) => {
         // setData(accelerometerData);
-        Accelerometer.setUpdateInterval(500);
+        Accelerometer.setUpdateInterval(100);
         x_arr.push(accelerometerData.x);
         y_arr.push(accelerometerData.y);
         z_arr.push(accelerometerData.z);
@@ -34,8 +37,12 @@ function BTTwo({ navigation }) {
         const y_sd = getStandardDeviation(y_arr);
         const z_sd = getStandardDeviation(z_arr);
         const sd = (x_sd + y_sd + z_sd) / 3;
+        // console.log("x: " + accelerometerData.x + " y:" + accelerometerData.y + " z:" + accelerometerData.z);
+        // console.log("x_sd: " + x_sd + " y_sd:" + y_sd + " z_sd:" + z_sd);
+        // console.log("sd:" + sd);
+        // console.log("\n");
         setData(sd);
-      }),
+      })
     );
   };
 
@@ -43,20 +50,24 @@ function BTTwo({ navigation }) {
     <SafeAreaView style={uiStyle.container}>
       <Text style={uiStyle.stackedText}>
         Hold to chest for 10 seconds after clicking "Start!" while keeping one
-        foot in front of the other {'\n'}
-        {'\n'}
+        foot in front of the other {"\n"}
+        {"\n"}
       </Text>
       <TouchableOpacity
         onPress={() => {
           if (!subscription) {
             _subscribe();
+            startedText();
+            setTimer(
+              setTimeout(() => {
+                Accelerometer.removeAllListeners();
+                Vibration.vibrate();
+                setSubscription(null);
+                resetText();
+                navigation.navigate("Balance Test 3");
+              }, 10000)
+            );
           }
-          changeText();
-          setTimeout(() => {
-            Accelerometer.removeAllListeners();
-            Vibration.vibrate();
-            navigation.navigate('Balance Test 3');
-          }, 10000);
         }}
         style={styles.startCheckButton}
       >
@@ -64,7 +75,12 @@ function BTTwo({ navigation }) {
       </TouchableOpacity>
       <View style={uiStyle.textContainer}>
         <TouchableOpacity
-          onPress={() => navigation.navigate('Balance Test 1')}
+          onPress={() => {
+            // console.log("Cancelled")
+            Accelerometer.removeAllListeners();
+            clearTimeout(timer);
+            navigation.navigate("Balance Test 1");
+          }}
           style={uiStyle.bottomButton}
         >
           <Text style={uiStyle.buttonLabel}>Cancel</Text>
@@ -74,37 +90,37 @@ function BTTwo({ navigation }) {
   );
 }
 
-const title = '#000000';
-const text = '#fff';
-const background = '#fff';
-const buttons = '#ff3333';
+const title = "#000000";
+const text = "#fff";
+const background = "#fff";
+const buttons = "#ff3333";
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     backgroundColor: background,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   startCheckButton: {
     width: 200,
     height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     borderRadius: 100,
     backgroundColor: buttons,
   },
   startCheckText: {
     color: text,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 20,
   },
   titleText: {
     color: title,
     fontSize: 30,
-    position: 'absolute',
+    position: "absolute",
     top: 60,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 });
 
