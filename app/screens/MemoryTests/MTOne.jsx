e5d47e391e4f7507fd0d1bdf0e22efb905fdd90b
Yupeng Zhang <yzha3836@uni.sydney.edu.au>
2022-08-23 21:03:17 +1000

    fix MTOne text alignment and button appearance

--- app/screens/MemoryTests/MTOne.jsx
+++ app/screens/MemoryTests/MTOne.jsx
@@ -20,8 +20,9 @@ function MTOne({ navigation }) {
   return (
     <View style={uiStyle.container}>
       <ScrollView>
-        <Text style={uiStyle.stackedText}>
-          Welcome to the first memory test. {'\n'}
+        <Text style={styles.text}>
+            {'\n'} {'\n'}
+          Welcome to the first memory test.{'\n'}
           {'\n'}
           The affected person will be presented with three images to remember.
           {'\n'}
@@ -29,13 +30,15 @@ function MTOne({ navigation }) {
           They will be tested on these images once now and then again at the end
           other assessments. {'\n'}
           {'\n'}
-          Pass the phone to the affected person.
+          Please pass the phone to the affected person. {'\n'}
+            {'\n'}
         </Text>
       </ScrollView>
 
       <TouchableOpacity
         onPress={() => navigation.navigate('Memory Test 2')}
-        style={uiStyle.bottomButton}
+        style={styles.bottomButton}
+        color
       >
         <Text style={uiStyle.buttonLabel}>Start!</Text>
       </TouchableOpacity>
@@ -43,6 +46,28 @@ function MTOne({ navigation }) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 25,
+        lineHeight: 25,
+        letterSpacing: 0.3,
+        marginHorizontal: 30,
+        marginVertical: 20,
+
+    },
+    bottomButton: {
+        // consistent with "View History" button on Home screen, i.e long red button on bottom
+        width: 300,
+        height: 50,
+        padding: 10,
+        borderRadius: 100,
+        backgroundColor: '#007AFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 50,
+        marginTop: 20,
+        alignSelf: 'center',
+    },
+});
 
 export default MTOne;
