9cf82bd7ef1e061511e47ddc2066383ce8a0ad12
Henriette Elise <henrietteelise@Henriettes-MacBook-Pro.local>
2022-08-28 15:26:56 +1000

    Fixed mistake with results

--- app/screens/ReactionTests/RTTwo.jsx
+++ app/screens/ReactionTests/RTTwo.jsx
@@ -12,8 +12,6 @@ import uiStyle from '../../components/uiStyle';
 import {
   IncidentReportRepoContext,
   ReportIdContext,
-  PreliminaryReportRepoContext,
-  PrelimReportIdContext
 } from '../../components/GlobalContextProvider';
 
 function getRandomInt(min, max) {
@@ -26,35 +24,43 @@ function RTTwo({ navigation }) {
   const [attemptResults, setAttemptResults] = useState([]);
   const [reportId] = useContext(ReportIdContext);
   const incidentRepoContext = useContext(IncidentReportRepoContext);
-  const preliminaryReportRepoContext = useContext(PreliminaryReportRepoContext);
-  const [prelimReportId] = useContext(PrelimReportIdContext);
 
   // Start time in milliseconds
   const [startMs, setStartMs] = useState(null);
 
-  // stage = button stage from start -> wait -> press
+  // Stage = button stage from start -> wait -> press
   const [stage, setStage] = useState(0);
 
   let btnStyle;
   let btnOnPress = () => {};
   let btnTxt;
+
+  // Stage 0 is the start stage, waiting for user to press button
   if (stage === 0) {
     btnStyle = styles.startButton;
     btnOnPress = () => {
       setStage(1);
       setTimeout(() => {
+
+        // Start timer as soon as the circle turns yellow
         setStartMs(Date.now());
         setStage(2);
-      }, getRandomInt(2000, 5000)); // wait between 2 and 5 seconds, then set to next stage
+      }, getRandomInt(2000, 5000)); // Wait between 2 and 5 seconds, then set to next stage
     };
     btnTxt = 'Start';
+
+  // Stage 1 is the wait stage
   } else if (stage === 1) {
     btnTxt = 'Wait...';
     btnStyle = styles.waitButton;
+
+  // Stage 2 is the press stage
   } else if (stage === 2) {
     btnTxt = 'Press!';
     btnStyle = styles.pressButton;
     btnOnPress = () => {
+
+      // Fix this later, maybe delete?
       setAttemptResults((prevAttemptResults) => [
         ...prevAttemptResults,
         Date.now() - startMs,
@@ -65,27 +71,22 @@ function RTTwo({ navigation }) {
     };
   }
 
+  // Check if user has failed or passed tests 
   useEffect(() => {
     if (attemptResults.length === 3) {
-      const avg =
-        attemptResults.reduce((a, b) => a + b) / attemptResults.length;
-      let grade = 'fail';
-      if (avg < 500) {
-        grade = 'pass';
+      let grade = 'pass';
+      if (attemptResults[0] >= 500 || attemptResults[1] >= 500 || 
+        attemptResults[2] >= 500) {
+            grade = 'fail';
       }
 
-      if(grade == 'fail'){
-        preliminaryReportRepoContext.updateReactionTestResult(prelimReportId,0);
-      }
-      else{
-        preliminaryReportRepoContext.updateReactionTestResult(prelimReportId,1); 
-      }
-      preliminaryReportRepoContext.getCurrentReportInformation(prelimReportId).then((data) => console.log(data));
+      let avg = 300; // FIX THIS SO IT'S NOT HARDCODED
+
       incidentRepoContext
         .setReactionTest(reportId, attemptResults, avg, grade)
         .catch(console.log);
       setAttemptResults([]);
-      navigation.navigate('Balance Test 1', {
+      navigation.navigate('Reaction Test 3', {
         attemptResults: attemptResults,
         avg: avg,
         grade: grade,
