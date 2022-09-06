e668e2f41f7bc70841a2bfce1235c6fa6137cd40
Dylan700 <dylanwilliams700@gmail.com>
2022-09-06 13:23:44 +1000

    Add zone conditional logic

--- app/screens/ActionPlanScreen.js
+++ app/screens/ActionPlanScreen.js
@@ -12,24 +12,24 @@ import ExpandableTab from '../components/ExpandableTab';
 import Badge from '../components/Badge';
 
 import uiStyle from '../components/uiStyle';
-
+import MyCheckbox from '../components/MyCheckbox';
 
 
 function ActionPlanScreen({ navigation }) {
 	const createAlert = () =>
-    Alert.alert(
-      'Alert',
-      'Over the next few days, symptoms may worsen or other symptoms may appear. Watch out for HEAD BUMPS. If they occur, seek urgent medical attention',
-      [
-        {
-          text: 'View Action Plan',
-          onPress: () => console.log('Cancel Pressed'),
-        },
-        {
-          text: 'Check Symptoms',
-          onPress: () => navigation.navigate('HEAD BUMPS'),
-        },
-      ],
+		Alert.alert(
+		'Alert',
+		'Over the next few days, symptoms may worsen or other symptoms may appear. Watch out for HEAD BUMPS. If they occur, seek urgent medical attention',
+		[
+			{
+			text: 'View Action Plan',
+			onPress: () => console.log('Cancel Pressed'),
+			},
+			{
+			text: 'Check Symptoms',
+			onPress: () => navigation.navigate('HEAD BUMPS'),
+			},
+		],
     );
 
 	const zones = {
@@ -51,6 +51,8 @@ function ActionPlanScreen({ navigation }) {
 		}
 	}
 	const [activeZone, setActiveZone] = useState(zones.red);
+	const [isSymptomTolerant, setIsSymptomTolerant] = useState(false);
+	const [isSymptomFree24Hours, setIsSymptomFree24Hours] = useState(false);
 
 	// whenever the data for injury date, doctor clearance or other symtom based information changes, update the zone.
 	useEffect(() => {
@@ -69,17 +71,27 @@ function ActionPlanScreen({ navigation }) {
 		}else{
 			setActiveZone(zones.orange);
 		}
+		
+		if(isSymptomTolerant){
+			setActiveZone(zones.yellow);
+		}
 
-	}, [])
+		if(isSymptomFree24Hours){
+			setActiveZone(zones.green);
+		}
+	}, [isSymptomFree24Hours, isSymptomTolerant])
+
+	useEffect(() => {
+		createAlert();
+	}, []);
 
 	return (
 		<SafeAreaView>
-			{createAlert()}
 			<ScrollView>
 				<Text style={[uiStyle.text, {textAlign: "center", marginBottom: 5}]}>Concussion Action Plan</Text>
 				<Badge color={activeZone.color}>{activeZone.name} ZONE</Badge>
 				<Text style={[{textAlign: "center", padding: 10}]}>Have your child complete the following zone and stepwise program. Aim to keep activity within the rating of perceived exertion (RPE) or heart rate guide (if your child has a heart rate monitoring device).{"\n\n"} Seek urgent medical attention if your child's symptoms worsen or if other symptoms appear.</Text>
-				<ExpandableTab title="Red Zone" subtitle="Days 1 and 2 following injury" color={activeZone == zones.red ? activeZone.color : "lightgrey"} footer="After 2 days of acute rest, you may move on to the next zone." header="RPE - Nothing at all, very very light. Heart rate < 120 bpm">
+				<ExpandableTab title="Red Zone" subtitle="Days 1 and 2 following injury" color={activeZone.name == zones.red.name ? activeZone.color : "lightgrey"} footer="After 2 days of acute rest, you may move on to the next zone." header="RPE - Nothing at all, very very light. Heart rate < 120 bpm">
 					<Text style={{fontWeight:"bold"}}>Rest your child from any physical or cognitive activity.{"\n\n\n"}</Text>
 					<Text style={{fontWeight:"bold"}}>Supportive Care:{"\n\n"}</Text>
 					<Text>
@@ -90,7 +102,7 @@ function ActionPlanScreen({ navigation }) {
 						- Encourage your child to have a positive mental attitude towards their recovery.
 					</Text>
 				</ExpandableTab>
-				<ExpandableTab title="Orange Zone" subtitle="Until cleared for light activity" color={activeZone == zones.orange ? activeZone.color : "lightgrey"} footer="See your GP to check that your child may progress to the next zone. Your child must be symptom tolerant before moving on to Step 1" header="RPE - Very light or light to moderate. Heart rate 120 - 140 bpm">
+				<ExpandableTab title="Orange Zone" subtitle="Until cleared for light activity" color={activeZone.name == zones.orange.name ? activeZone.color : "lightgrey"} footer="See your GP to check that your child may progress to the next zone. Your child must be symptom tolerant before moving on to Step 1" header="RPE - Very light or light to moderate. Heart rate 120 - 140 bpm">
 					<Text style={{fontWeight:"bold"}}>Reccomendations:{"\n\n"}</Text>
 					<Text>
 						- Start low level physical and cognitive activity. Your child can now move around more freely.{"\n\n"}
@@ -104,9 +116,13 @@ function ActionPlanScreen({ navigation }) {
 						- Try to reduce and/or stop headache medication once your child is more physically/mentally active.{"\n\n"}
 						- Should sleep pattern remain a problem, then further assessment and possible treatment with Melatonin may be considered.
 						This will require medical supervision and is best discussed with your local GP.
+						{"\n"}
 					</Text>
+					<View style={styles.checkboxContainer}>
+						<MyCheckbox onUpdate={(val) => setIsSymptomTolerant(val)}/><Text style={styles.checkboxLabel}>{`My child is Symptom Tolerant`}</Text>
+					</View>
 				</ExpandableTab>
-				<ExpandableTab title="Yellow Zone" subtitle="GRADED RETURN TO ACTIVITY" color={activeZone == zones.yellow ? activeZone.color : "lightgrey"} footer="Progress to the next step if your child is symptom free for 24 hours." header="RPE - Moderate to Hard. Heart rate 80% HRt +10% each week">
+				<ExpandableTab title="Yellow Zone" subtitle="GRADED RETURN TO ACTIVITY" color={activeZone.name == zones.yellow.name ? activeZone.color : "lightgrey"} footer="Progress to the next step if your child is symptom free for 24 hours." header="RPE - Moderate to Hard. Heart rate 80% HRt +10% each week">
 					<Text style={{fontWeight:"bold"}}>Step 1 - Light cognitive and physical activity{"\n"}</Text>
 					<Text>
 						{"\n\t"}- Progress toward 30 minutes of cognitive exertion.
@@ -123,9 +139,13 @@ function ActionPlanScreen({ navigation }) {
 					<Text>
 						{"\n\t"}- Progress towards full time school with minimal accommodations.
 						{"\n\t"}- More intense aerobic and skill-based activity on a more regular basis. 
+						{"\n"}
 					</Text>
+					<View style={styles.checkboxContainer}>
+						<MyCheckbox onUpdate={(val) => setIsSymptomFree24Hours(val)}/><Text style={styles.checkboxLabel}>{`My child has been symptom free for 24 hours.`}</Text>
+					</View>
 				</ExpandableTab>
-				<ExpandableTab title="Green Zone" subtitle="RETURNING TO PRE-INJURY ACTIVITY" color={activeZone == zones.green ? activeZone.color : "lightgrey"} header="RPE - Very Hard to Maximal. Heart rate 80 - 100% HRt">
+				<ExpandableTab title="Green Zone" subtitle="RETURNING TO PRE-INJURY ACTIVITY" color={activeZone.name == zones.green.name ? activeZone.color : "lightgrey"} header="RPE - Very Hard to Maximal. Heart rate 80 - 100% HRt">
 					<Text>Once your child has been cleared to commence a return to activity protocol, they are ready to progress as follows:{"\n"}</Text>
 						<Text style={{fontWeight:"bold"}}>{"\n\n"}Step 4 - Pre-injury activity (without contact){"\n"}</Text>
 						<Text>
@@ -175,6 +195,21 @@ const styles = StyleSheet.create({
 		backgroundColor: '#ff0000',
 		alignItems: 'center',
 		justifyContent: 'center',
+	  },
+
+	checkboxLabel: {
+		marginLeft: 8,
+		fontWeight: '500',
+		fontSize: 14,
+		flex: 1,
+		flexWrap: 'wrap',
+	},
+
+	checkboxContainer: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		margin: 1,
+		padding: 5,
 	  },
 });
 
