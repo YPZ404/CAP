1ad5eb505abd3288dedc44a4244377ebfab30a78
Yupeng Zhang <yzha3836@uni.sydney.edu.au>
2022-09-16 11:29:01 +1000

    Add more tests for MyCheckbox

--- app/components/__test__/MyCheckBox.test.js
+++ app/components/__test__/MyCheckBox.test.js
@@ -9,4 +9,40 @@ describe("<MyCheckbox />", () => {
         expect(screen.queryByTestId("checkmark")).toBeNull();
     });
 
+    it("is checked when pressed", () => {
+        render(<MyCheckbox />);
+        fireEvent.click(screen.getByTestId("checkbox"));
+        expect(screen.queryByTestId("checkmark")).not.toBeNull();
+    });
+
+    it("is unchecked when pressed twice", () => {
+        render(<MyCheckbox />);
+        fireEvent.click(screen.getByTestId("checkmark"));
+        fireEvent.click(screen.getByTestId("checkmark"));
+        expect(screen.queryByTestId("checkmark")).toBeNull();
+    });
+
+    it("calls onUpdate when pressed", () => {
+        const onUpdate = jest.fn();
+        render(<MyCheckbox onUpdate={onUpdate} />);
+        expect(onUpdate).toHaveBeenCalledTimes(0);
+        fireEvent.click(screen.getByTestId("checkmark"));
+        expect(onUpdate).toHaveBeenCalledTimes(1);
+    });
+
+    it("has no checkmark when checked is false", () => {
+        render(<MyCheckbox checked={false} />);
+        expect(screen.queryByTestId("checkmark")).toBeNull();
+    });
+
+    it("has a checkmark when checked is true", () => {
+        render(<MyCheckbox checked={true} />);
+        fireEvent.click(screen.getByTestId("checkmark"));
+        expect(screen.queryByTestId("checkmark")).not.toBeNull();
+    });
+
+    it("has a transparent background when checked is false", () => {
+        render(<MyCheckbox checked={false} />);
+        expect(screen.queryByTestId("checkbox").props.style[0].backgroundColor).toBe("transparent");
+    });
 });
