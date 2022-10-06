436ccba245193585c0664feacdb001bf43456cb6
mkar6206 <mkar6206@uni.sydney.edu.au>
2022-10-06 13:52:41 +1100

    fixed data saving issue

--- app/screens/DSLScreen.js
+++ app/screens/DSLScreen.js
@@ -287,6 +287,11 @@ function DSLScreen({ navigation }) {
               sliderSevenValue+sliderEightValue+sliderNineValue+sliderTenValue+sliderElevenValue+
               sliderTwelveValue + sliderThirteenValue+sliderFourteenValue+sliderFifteenValue+sliderSixteenValue+
               sliderSeventeenValue+sliderEighteenValue+sliderNineteenValue+sliderTwentyValue;
+              
+            preliminaryReportRepoContext.createDSL(account.account_id, sliderOneValue, sliderTwoValue, sliderThreeValue, sliderFourValue, sliderFiveValue, sliderSixValue, sliderSevenValue,sliderEightValue,sliderNineValue,sliderTenValue,sliderElevenValue,
+                sliderTwelveValue, sliderThirteenValue,sliderFourteenValue, sliderFifteenValue,sliderSixteenValue,
+                sliderSeventeenValue,sliderEighteenValue,sliderNineteenValue,sliderTwentyValue, totalSliderValue).then((data)=>setDSLId(data));
+              
             setSliderOneValue(0);
             setSliderTwoValue(0);
             setSliderThreeValue(0);
@@ -330,9 +335,7 @@ function DSLScreen({ navigation }) {
 
 
             // Add necessary stuff for user @mariam :)
-            preliminaryReportRepoContext.createDSL(account.account_id, sliderOneValue, sliderTwoValue, sliderThreeValue, sliderFourValue, sliderFiveValue, sliderSixValue, sliderSevenValue,sliderEightValue,sliderNineValue,sliderTenValue,sliderElevenValue,
-              sliderTwelveValue,  sliderThirteenValue,sliderFifteenValue,sliderSixteenValue,
-              sliderSeventeenValue,sliderEighteenValue,sliderNineteenValue,sliderTwentyValue, totalSliderValue).then((data)=>setDSLId(data));
+            
             navigation.navigate('Continue Tests', { screen: 'DSL Complete'});
            
 
