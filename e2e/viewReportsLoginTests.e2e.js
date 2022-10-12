e7b6b9874889e6d23d3e81f91f4d708c2ce335c3
Henriette Onarheim <hona3215@uni.sydney.edu.au>
2022-10-12 17:32:50 +1100

    Added 'View Reports' tests

---
+++ e2e/viewReportsLoginTests.e2e.js
@@ -0,0 +1,128 @@
+
+describe('Example', () => {
+    beforeAll(async () => {
+      await device.launchApp();
+    });
+    
+
+    // Testing how the "View Reports" page looks like when logged in
+    beforeEach(async () => {
+        await device.reloadReactNative();
+        await element(by.text('I understand')).tap();
+
+        // Signing up and logging in
+        await element(by.text('View Reports')).tap();
+        await element(by.text('Preliminary Test Reports')).tap();
+        await element(by.text('OK')).tap();
+        await element(by.text('Sign Up')).tap();
+        await element(by.type('RCTUITextField')).atIndex(0).typeText('William');
+        await element(by.type('RCTUITextField')).atIndex(1).typeText('Johnson');
+        await element(by.type('RCTUITextField')).atIndex(2).typeText('16');
+        await element(by.type('RCTUITextField')).atIndex(3).typeText('80');
+        await element(by.type('RCTUITextField')).atIndex(4).typeText('hey123');
+        await element(by.text('Submit')).tap();
+        await element(by.type('RCTRootContentView')).swipe('right', 'fast', 0.5, 0.01, 0.01);
+        await element(by.text('Login')).tap();
+        await element(by.type('RCTUITextField')).atIndex(0).typeText('William');
+        await element(by.type('RCTUITextField')).atIndex(1).typeText('Johnson');
+        await element(by.type('RCTUITextField')).atIndex(2).typeText('hey123');
+        await element(by.text('Submit')).tap();
+        await element(by.text('View Reports')).tap();
+    });
+
+    /* // If user is logged in, check that they can see incident reports
+    it('User can login to see incident reports in View Reports', async () => {
+
+        // Check that there are no incident reports
+        await element(by.text('Incident Reports')).tap();
+        await expect(element(by.text('All Incident Reports for William'))).toBeVisible();
+        await expect(element(by.text('Return to Home'))).toBeVisible();
+    });
+
+    // If user is logged in, check that they can see preliminary reports
+    it('User should be able to see preliminary reports in View Reports when logged in', async () => {
+
+      // Check that there are no preliminary test reports
+      await element(by.text('Preliminary Test Reports')).tap();
+      await expect(element(by.text('All Preliminary Reports for William'))).toBeVisible();
+      await expect(element(by.text('No such reports.'))).toBeVisible();
+  }); */
+
+    // If user is logged in, check that they can see preliminary reports
+    it('User should see prelim reports after doing prelim tests', async () => {
+
+        // Check that there are no preliminary test reports
+        await element(by.type('RCTRootContentView')).swipe('right', 'fast', 0.5, 0.01, 0.01);
+        await element(by.text('Preliminary Tests')).tap();
+        await element(by.text('Start!')).tap();
+        await element(by.text('Start!')).tap();
+        await element(by.text('Next')).tap();
+        await element(by.text('Next')).tap();
+        await element(by.text('Next')).tap();
+        await element(by.text('Next')).tap();
+        await element(by.text('Submit')).tap();
+        await element(by.text('Next')).tap();
+        await element(by.type('RCTRootContentView')).tap({x: 200, y: 500});
+
+        // Perform reaction tests
+        await waitFor(element(by.text('Press!'))).toBeVisible().withTimeout(7000).then(async () => {
+            await element(by.text('Press!')).tap();
+        });
+        await element(by.type('RCTRootContentView')).tap({x: 200, y: 500});
+
+        await waitFor(element(by.text('Press!'))).toBeVisible().withTimeout(7000).then(async () => {
+            await element(by.text('Press!')).tap();
+        });
+        await element(by.type('RCTRootContentView')).tap({x: 200, y: 500});
+
+        await waitFor(element(by.text('Press!'))).toBeVisible().withTimeout(7000).then(async () => {
+            await element(by.text('Press!')).tap();
+        });
+        await element(by.type('RCTRootContentView')).tap({x: 200, y: 500});
+        await element(by.text('Next')).tap();
+
+        // Perform balance tests
+        await element(by.text('Start!')).tap();
+        await waitFor(element(by.text('Next'))).toBeVisible().withTimeout(10000).then(async () => {
+            await element(by.text('Next')).tap();
+        });
+        await element(by.text('Start!')).tap();
+
+        await waitFor(element(by.text('Next'))).toBeVisible().withTimeout(10000).then(async () => {
+            await element(by.text('Next')).tap();
+        });
+        await element(by.text('Next')).tap();
+        await element(by.text('Next')).tap();
+        await element(by.text('Start!')).tap();
+
+        await waitFor(element(by.text('Alert'))).toBeVisible().withTimeout(15000).then(async () => {
+            await element(by.text('Yes')).tap();
+        });
+        await element(by.text('Next')).tap();
+        await element(by.text('Next')).tap();
+        await element(by.text('Next')).tap();
+        await element(by.text('Submit')).tap();
+
+        // Save test results to profile
+        await element(by.text('Save Report')).tap();
+        await element(by.text('Save to logged profile')).tap();
+
+        // See prelim test results
+        await element(by.text('View Reports')).tap();
+        await element(by.text('Preliminary Test Reports')).tap();
+
+        // Ignore balance tests, check if results are added to profile
+        await element(by.type('RCTRootContentView')).swipe('right', 'fast', 0.5, 0.01, 0.01);
+        await element(by.text('Home Page')).tap();
+        await element(by.text('View Reports')).tap();
+        await element(by.text('Preliminary Test Reports')).tap();
+        await expect(element(by.text('Memory Test 1: FAIL'))).toBeVisible();
+        await expect(element(by.text('Memory Test 2: FAIL'))).toBeVisible();
+        await expect(element(by.text('Reaction Test: PASS'))).toBeVisible();
+        await expect(element(by.text('Balance Test 1: UNDEFINED'))).toBeVisible();
+        await expect(element(by.text('Balance Test 1: UNDEFINED'))).toBeVisible();
+
+        await device.disableSynchronization();
+    });
+});
+  
--- e2e/viewReportsTests.e2e.js
+++ e2e/viewReportsTests.e2e.js
@@ -2,7 +2,8 @@ describe('Example', () => {
     beforeAll(async () => {
       await device.launchApp();
     });
-  
+    
+    // Testing how the "View Reports" page looks like when NOT logged in
     beforeEach(async () => {
       await device.reloadReactNative();
       await element(by.text('I understand')).tap();
@@ -13,11 +14,11 @@ describe('Example', () => {
         await element(by.text('View Reports')).tap();
         await expect(element(by.text('Which reports would you like to access?'))).toBeVisible();
         await expect(element(by.text('Preliminary Test Reports'))).toBeVisible();
-        await expect(element(by.text('Incident Reports'))).toBeVisible();
+        await expect(element(by.text('Daily Symptom Reports'))).toBeVisible();
     });
 
     // If user not logged in, check that they get taken to login page when pressing "Preliminary Test Reports"
-    it('when not logged in, should be taken to login page when trying to access reports', async () => {
+    it('when not logged in, should be taken to login page when trying to access prelim', async () => {
         await element(by.text('View Reports')).tap();
         await element(by.text('Preliminary Test Reports')).tap();
         await expect(element(by.text('Alert'))).toBeVisible();
@@ -32,61 +33,20 @@ describe('Example', () => {
         await expect(element(by.text('Sign Up'))).toBeVisible();
     });
 
-    // If user is logged in, check that they can see incident reports
-    it('User can login to see incident reports in View Reports', async () => {
-        await element(by.text('View Reports')).tap();
-        await element(by.text('Preliminary Test Reports')).tap();
-        await element(by.text('OK')).tap();
-        await element(by.text('Sign Up')).tap();
-
-        // Signing up
-        await element(by.type('RCTUITextField')).atIndex(0).typeText('William');
-        await element(by.type('RCTUITextField')).atIndex(1).typeText('Johnson');
-        await element(by.type('RCTUITextField')).atIndex(2).typeText('16');
-        await element(by.type('RCTUITextField')).atIndex(3).typeText('80');
-        await element(by.type('RCTUITextField')).atIndex(4).typeText('hey123');
-        await element(by.text('Submit')).tap();
-        await element(by.type('RCTRootContentView')).swipe('right', 'fast', 0.5, 0.01, 0.01);
-        await element(by.text('Login')).tap();
-        await element(by.type('RCTUITextField')).atIndex(0).typeText('William');
-        await element(by.type('RCTUITextField')).atIndex(1).typeText('Johnson');
-        await element(by.type('RCTUITextField')).atIndex(2).typeText('hey123');
-        await element(by.text('Submit')).tap();
-        await element(by.text('View Reports')).tap();
-
-        // Check that there are no incident reports
-        await element(by.text('Incident Reports')).tap();
-        await expect(element(by.text('All Incident Reports for William'))).toBeVisible();
-        await expect(element(by.text('Return to Home'))).toBeVisible();
-    });
-
-    // If user is logged in, check that they can see preliminary reports
-    it('User should be able to see preliminary reports in View Reports when logged in', async () => {
-      jest.setTimeout(100000);
+    // If user not logged in, check that they get taken to login page when pressing "Preliminary Test Reports"
+    it('when not logged in, should be taken to login page when trying to access DSR', async () => {
       await element(by.text('View Reports')).tap();
-      await element(by.text('Preliminary Test Reports')).tap();
+      await element(by.text('Daily Symptom Reports')).tap();
+      await expect(element(by.text('Alert'))).toBeVisible();
+      await expect(element(by.text('Need to Login to see reports'))).toBeVisible();
+      await expect(element(by.text('OK'))).toBeVisible();
       await element(by.text('OK')).tap();
-      await element(by.text('Sign Up')).tap();
-
-      // Signing up
-      await element(by.type('RCTUITextField')).atIndex(0).typeText('William');
-      await element(by.type('RCTUITextField')).atIndex(1).typeText('Johnson');
-      await element(by.type('RCTUITextField')).atIndex(2).typeText('16');
-      await element(by.type('RCTUITextField')).atIndex(3).typeText('80');
-      await element(by.type('RCTUITextField')).atIndex(4).typeText('hey123');
-      await element(by.text('Submit')).tap();
-      await element(by.type('RCTRootContentView')).swipe('right', 'fast', 0.5, 0.01, 0.01);
-      await element(by.text('Login')).tap();
-      await element(by.type('RCTUITextField')).atIndex(0).typeText('William');
-      await element(by.type('RCTUITextField')).atIndex(1).typeText('Johnson');
-      await element(by.type('RCTUITextField')).atIndex(2).typeText('hey123');
-      await element(by.text('Submit')).tap();
-      await element(by.text('View Reports')).tap();
-
-      // Check that there are no preliminary test reports
-      await element(by.text('Preliminary Test Reports')).tap();
-      await expect(element(by.text('All Preliminary Reports for William'))).toBeVisible();
-      await expect(element(by.text('No such reports.'))).toBeVisible();
+      await expect(element(by.text('Enter your first name and last name to login'))).toBeVisible();
+      await expect(element(by.text('First Name'))).toBeVisible();
+      await expect(element(by.text('Last Name'))).toBeVisible();
+      await expect(element(by.text('Password'))).toBeVisible();
+      await expect(element(by.text('Submit'))).toBeVisible();
+      await expect(element(by.text('Sign Up'))).toBeVisible();
   });
 });
   
